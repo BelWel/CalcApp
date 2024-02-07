9e14db5b4cad77ed8a9f50db2cb5ec5869889578
Bel Welde <belsoft23@gmail.com>
2024-02-06 20:09:44 -0500

    Initial Commit

---
+++ calcApp/calc.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    pass
+
+
+def subtract(a, b):
+    pass
+
+
+def multiply(a, b):
+    pass
+
+
+def divide(a, b):
+    pass
+
+
+print(add(6, 4))
+print(subtract(10, 5))
+print(multiply(3, 4))
+print(divide(6, 3))
